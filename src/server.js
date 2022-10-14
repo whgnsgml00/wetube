d7bc17dddb33e4b21fd6cdc4a595a036073a444d
Richard <whgnsgml00@naver.com>
2022-10-14 18:33:57 +0900

    second commit

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const logger = morgan("dev");
 app.use(logger);
 
 app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views")
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
